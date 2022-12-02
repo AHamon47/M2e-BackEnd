f781f7821b82bf2dacf698181e8d15a496e0cbfc
AHamon47 <ahamon47@gmail.com>
2022-12-02 11:12:18 +0100

    Initial commit

---
+++ src/main/java/com/messagerie/M2e/M2eApplication.java
@@ -0,0 +1,13 @@
+package com.messagerie.M2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2eApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messagerie/M2e/M2eApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagerie.M2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
