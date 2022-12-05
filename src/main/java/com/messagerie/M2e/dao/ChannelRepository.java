8f7205d9402e8eeee64da4246ad0200fcafff437
webEtu <azadeh.ghasemizadeh2022@campus-eni.fr>
2022-12-05 14:52:03 +0100

    edit import to MessageController + add annotation to ChannelRepository

--- src/main/java/com/messagerie/M2e/dao/ChannelRepository.java
+++ src/main/java/com/messagerie/M2e/dao/ChannelRepository.java
@@ -1,8 +1,10 @@
 package com.messagerie.M2e.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.messagerie.M2e.model.Channel;
+import org.springframework.stereotype.Repository;
 
+import com.messagerie.M2e.model.Channel;
+@Repository
 public interface ChannelRepository extends JpaRepository<Channel, Integer> {
 
 }
